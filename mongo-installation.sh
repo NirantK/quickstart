7e5cde4abfdae19710eeac73477760f816a503e5
Nirant <NirantK@users.noreply.github.com>
2019-06-25 12:58:11 +0530

    Update and rename mongo-installation.md to mongo-installation.sh

--- mongo-installation.sh
+++ mongo-installation.sh
@@ -1,4 +1,3 @@
-```bash
 sudo apt-get install dirmngr
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
@@ -9,4 +8,3 @@ echo "mongodb-org-server hold" | sudo dpkg --set-selections
 echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-```
