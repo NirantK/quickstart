7021eb9599734a5d2991125ab85a7dd95c67a2bb
Nirant <NirantK@users.noreply.github.com>
2019-06-25 13:07:06 +0530

    Update mongo-installation.sh

--- mongo-installation.sh
+++ mongo-installation.sh
@@ -1,6 +1,7 @@
 sudo apt-get install dirmngr
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.listsudo apt-get update
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
 sudo apt-get install -y mongodb-org
 echo "mongodb-org hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
