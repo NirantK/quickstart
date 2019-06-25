d730b40d883cf3e68ba4c6fd4b10a0f98460b035
Nirant <NirantK@users.noreply.github.com>
2019-06-25 13:13:50 +0530

    Update mongo-installation.sh

--- mongo-installation.sh
+++ mongo-installation.sh
@@ -8,3 +8,4 @@ echo "mongodb-org-server hold" | sudo dpkg --set-selections
 echo "mongodb-org-shell hold" | sudo dpkg --set-selections
 echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
 echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+pip install pymongo
